c2bc01417953cf6695a69cbc4899046f2cc3df61
Ilya Poltawezc <ilya.poltawezc@gmail.com>
2025-08-03 17:09:30 +0300

    JS module slader, + adaptive

--- scripts/Slader.js
+++ scripts/Slader.js
@@ -9,8 +9,11 @@ class Slader {
 
     stateClasses = {
         imgOne: 'imgOne-translate',
+        deleteClassOne: 'what__body-img1',
         img2: 'img2',
+        deleteClassThree: 'what__body-img2',
         img3: 'img3',
+        deleteClassTwo: 'what__body-img3',
     }
 
     constructor() {
@@ -23,8 +26,14 @@ class Slader {
     }
 
     buttonClick = () => {
+        this.images1Element.classList.toggle(this.stateClasses.deleteClassOne)
         this.images1Element.classList.toggle(this.stateClasses.imgOne)
+        this.images2Element.classList.toggle(this.stateClasses.deleteClassThree)
+        this.images2Element.classList.toggle(this.stateClasses.img2)
+        this.images3Element.classList.toggle(this.stateClasses.deleteClassTwo)
         this.images3Element.classList.toggle(this.stateClasses.img3)
+
+
     }
 
     bindEvents() {
